e7f16cfd92d67d845c6ce2f6610e62e92ce53ead
khanh <khanhdacodau2007@gmail.com>
2025-10-11 13:26:00 +0700

    remove all

--- src/utils/formatPrice.ts
+++
@@ -1,5 +0,0 @@
-export function formatPrice(v: number) {
-  // Simple VND formatter without Intl to keep it tree‑shakable
-  const s = Math.round(v).toString();
-  return s.replace(/\\B(?=(\\d{3})+(?!\\d))/g, ".") + "đ";
-}
