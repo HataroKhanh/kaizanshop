bdd2bc2bf269b9b15927d6d3476f736a61a713ba
khanh <khanhdacodau2007@gmail.com>
2025-10-19 09:23:47 +0700

    #

--- app/api/products/push_new_product/route.ts
+++ app/api/products/push_new_product/route.ts
@@ -16,29 +16,6 @@ export async function POST(req: Request) {
   const arrayBuf = await file.arrayBuffer();
 
   const buf = Buffer.from(arrayBuf);
-  const origName = file.name || "upload.bin";
-  const mimeType = file.type || "application/octet-stream";
 
-  console.log(form);
-  return NextResponse.json(buf);
-  const auth = new google.auth.JWT({
-    email: process.env.GSA_CLIENT_EMAIL!,
-    key: process.env.GSA_PRIVATE_KEY!.replace(/\\n/g, "\n"),
-    scopes: ["https://www.googleapis.com/auth/drive.file"],
-  });
 
-  const drive = google.drive({ version: "v3", auth });
-
-  await drive.files.create({
-    requestBody: {
-      name: "big.mp4",
-      parents: ["<FOLDER_ID>"],
-    },
-    media: {
-      mimeType: "video/mp4",
-      body: fs.createReadStream("./big.mp4"),
-    },
-    fields: "id",
-  });
-  return NextResponse.json({});
 }
