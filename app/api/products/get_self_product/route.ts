ae761a6df2c74c197a366d177b1dbec17536f0c2
khanh <khanhdacodau2007@gmail.com>
2025-10-21 11:54:47 +0700

    add error code to response

--- app/api/products/get_self_product/route.ts
+++ app/api/products/get_self_product/route.ts
@@ -6,9 +6,12 @@ import { authOptions } from "@/auth.config";
 export async function GET(req: Request) {
   const session = await getServerSession(authOptions);
   if (!session)
-    return NextResponse.json("Fail to get product, please login to continue", {
-      status: 401,
-    });
+    return NextResponse.json(
+      { error: "Fail to get product, please login to continue" },
+      {
+        status: 401,
+      }
+    );
 
   const { searchParams } = new URL(req.url);
 
@@ -18,22 +21,28 @@ export async function GET(req: Request) {
   const db = client.db("kaizanshop");
   const products = db.collection("products");
 
-  const product = await products.findOne({ idProduct: id },{projection : {
-    _id : 0,
-    idProduct : 0,
-    "owner.email" : 0,
-
-  }});
+  const product = await products.findOne(
+    { idProduct: id },
+    {
+      projection: {
+        _id: 0,
+        idProduct: 0,
+        "owner.email": 0,
+      },
+    }
+  );
 
   //Check if id belongs to the owner
   // const productParse = JSON.parse(product ? product : "")
-  if (!product) return NextResponse.json("Product not found", { status: 404 });
+  if (!product)
+    return NextResponse.json({ error: "Product not found" }, { status: 404 });
 
-console.log(product,session.user.id);
-    
   const check: Boolean = product.owner.id === session.user.id;
   if (!check)
-    return NextResponse.json("You are not the owner of this product", { status: 401 });
+    return NextResponse.json(
+      { error: "You are not the owner of this product" },
+      { status: 401 }
+    );
 
   return NextResponse.json(product);
 }
